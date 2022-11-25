56fe3437ff7635a394ee381d65a605653a37997b
Arjun-93 <88470385+Arjun-93@users.noreply.github.com>
2022-11-25 22:13:39 +0530

    Add files via upload

---
+++ Tank_Star_Game/PlayScreenGUI.java
@@ -0,0 +1,9 @@
+package com.mygdx.game.test2;
+
+import javax.swing.*;
+
+public class PlayScreenGUI {
+    private JProgressBar progressBar1;
+    private JProgressBar progressBar2;
+    private JTextPane textPane1;
+}
---
+++ Tank_Star_Game/TankSatrGame.java
@@ -0,0 +1,11 @@
+package com.mygdx.game.test2;
+
+import com.badlogic.gdx.Game;
+
+public class TankSatrGame extends Game {
+
+    @Override
+    public void create() {
+
+    }
+}
